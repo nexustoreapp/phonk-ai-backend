8c45c6132ac2be0333c7712e417d885599a27e97
Edson <edsonbritonjunior@gmail.com>
2026-02-03 06:30:07 -0400

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import os
+import uuid
 
 app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
+
 @app.post("/upload-audio")
 async def upload_audio(file: UploadFile = File(...)):
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="Arquivo inválido")
+
+    file_id = f"{uuid.uuid4()}_{file.filename}"
+    file_path = os.path.join(UPLOAD_DIR, file_id)
+
+    with open(file_path, "wb") as f:
+        content = await file.read()
+        f.write(content)
+
     return {
-        "filename": file.filename,
-        "content_type": file.content_type
+        "message": "upload ok",
+        "file_id": file_id,
+        "path": file_path
     }
