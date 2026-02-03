98ea57691e8770a51d53424925faf997c8d81801
Edson <edsonbritonjunior@gmail.com>
2026-02-03 06:07:50 -0400

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def root():
